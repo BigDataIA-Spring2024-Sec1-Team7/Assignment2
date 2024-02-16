73198291c2deff391ac236a3a41dd6c2f39907c2
rutujamanoharkute <kute.r@northeastern.edu>
2024-02-16 14:26:32 -0500

    Deleted unnecessary files
    Renamed snowflake2 to load_to_snowflake

--- load_to_snowflake.py
+++ load_to_snowflake.py
@@ -1,58 +1,119 @@
-import os
+import sqlalchemy
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
+import os
+from sqlalchemy.engine import URL
+import pandas as pd
+import re
 
-# Load environment variables from .env file
 load_dotenv()
 
-def create_snowflake_engine(query):
-    """
-    Creates and returns a SQLAlchemy engine for Snowflake.
+base_url = URL.create(
+    "snowflake",
+    username=os.getenv('SNOWFLAKE_USER'),
+    password=os.getenv('SNOWFLAKE_PASS'),
+    host=os.getenv('SNOWFLAKE_ACC_ID'),
+)
 
-    Returns:
-    - engine: SQLAlchemy engine object
+# Creating database for storing cfa data
+create_database_query = "CREATE OR REPLACE DATABASE all_files;"
+create_warehouse_query = """CREATE OR REPLACE WAREHOUSE all_wh WITH
+    WAREHOUSE_SIZE = 'X-SMALL'
+    AUTO_SUSPEND = 180
+    AUTO_RESUME = TRUE
+    INITIALLY_SUSPENDED = TRUE; 
+"""
 
-    """
-    SNOWFLAKE_ACCOUNT=os.getenv("SNOWFLAKE_ACCOUNT")
-    SNOWFLAKE_USER=os.getenv("SNOWFLAKE_USER")
-    SNOWFLAKE_PASSWORD=os.getenv("SNOWFLAKE_PASSWORD")
+# Creating table for scraped data
 
+
+def executeDDLQueries(connection):
+    connection.execute(create_database_query)
+    connection.execute(create_warehouse_query)
+  
+
+engine = create_engine(base_url)
+
+try:
+    connection = engine.connect()
+    executeDDLQueries(connection=connection)
+
+except Exception as e:
+    print(e)
+# finally:
+    # connection.close()
+    # engine.dispose()
+
+def insert_data_to_snowflake(file_path, link, table):
     try:
-        # Create SQLAlchemy engine
-        engine = create_engine(f'snowflake://{SNOWFLAKE_USER}:{SNOWFLAKE_PASSWORD}@{SNOWFLAKE_ACCOUNT}/')
-        # connection = engine.connect()
-        # result = connection.execute(query)
-        return engine
+        # Creating table for scraped data
+        base_url = URL.create(
+        "snowflake",
+        username=os.getenv('SNOWFLAKE_USER'),
+        password=os.getenv('SNOWFLAKE_PASS'),
+        host=os.getenv('SNOWFLAKE_ACC_ID'),
+        )
+        engine = create_engine(base_url)
+        connection = engine.connect()
+        connection.execute("USE DATABASE all_files")
+        connection.execute("USE WAREHOUSE all_wh")
+        create_data_table_query = f"""CREATE TABLE IF NOT EXISTS {table} (
+            text_column TEXT,
+            link_column TEXT
+           );
+        """
+        connection.execute(create_data_table_query)
+
+        # Read the text file
+        with open(file_path, 'r') as file:
+            text_data = file.read()
+            text_data = str(text_data) 
+            re.sub(r'[\'"‘’”“]', '', text_data)
+        link = str(link)
+            
+      
+        
+        
+     
+        insert_query = f"""INSERT INTO {table} (text_column, link_column) VALUES('''{text_data}''', '{link}');"""
+        print(insert_query)
+    
+
+        connection.execute(insert_query)
+
+        print("Data inserted successfully.")
     except Exception as e:
-        print(f"Error creating Snowflake engine: {e}")
-        return None, None
+        print("Error:", e)
 
 # Example usage:
-query = "SELECT current_version()"
-engine = create_snowflake_engine(query)
+file_path = './PyPDF_RR_2024_l1_combined.txt'
+link = 's3://s3-assignment2/files_txt/PyPDF_RR_2024_l1_combined.txt'
+table = 'PyPDF'
+insert_data_to_snowflake(file_path, link, table)
+
+file_path = './PyPDF_RR_2024_l2_combined.txt'
+link = 's3://s3-assignment2/files_txt/PyPDF_RR_2024_l2_combined.txt'
+insert_data_to_snowflake(file_path, link, table)
+
+file_path = './PyPDF_RR_2024_l3_combined.txt'
+link = 's3://s3-assignment2/files_txt/PyPDF_RR_2024_l3_combined.txt'
+insert_data_to_snowflake(file_path, link, table)
 
 
-if engine:
-    print("Snowflake engine created successfully.")
-    print(engine)
-else:
-    print("Failed to create Snowflake engine.")
+file_path = './Grobid_RR_2024_l1_combined.txt'
+link = 's3://s3-assignment2/files_txt/Grobid_RR_2024_l1_combined.txt'
+table = 'GROBID'
+insert_data_to_snowflake(file_path, link, table)
 
-# if result:
-#     print(result[0])
-# else:
-#     print("error")
-    
-connection = engine.connect()
-result = connection.execute(query)
-print(result[0])
+file_path = './Grobid_RR_2024_l2_combined.txt'
+link = 's3://s3-assignment2/files_txt/Grobid_RR_2024_l2_combined.txt'
+insert_data_to_snowflake(file_path, link, table)
 
+file_path = './Grobid_RR_2024_l3_combined.txt'
+link = 's3://s3-assignment2/files_txt/Grobid_RR_2024_l3_combined.txt'
+insert_data_to_snowflake(file_path, link, table)
 
-
-
-   
-        
-        
-
+connection.close()
+engine.dispose()
 
 
